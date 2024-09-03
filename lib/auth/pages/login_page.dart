67316f71c24f77d0b4de781905a47e5835cea300
Eduir Brazon <eduirbrazon@gmail.com>
2024-09-03 17:59:23 -0400

    se mejora la forma de servir las paginas

--- lib/auth/pages/login_page.dart
+++ lib/auth/pages/login_page.dart
@@ -15,73 +15,70 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.surface,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            //logo
-            Icon(
-              Icons.message,
-              size: 60,
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          //logo
+          Icon(
+            Icons.message,
+            size: 60,
+            color: Theme.of(context).colorScheme.primary,
+          ),
+          //wbtext
+          Text(
+            "Wb you've been missed",
+            style: TextStyle(
               color: Theme.of(context).colorScheme.primary,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
             ),
-            //wbtext
-            Text(
-              "Wb you've been missed",
-              style: TextStyle(
-                color: Theme.of(context).colorScheme.primary,
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          //email field
+          MyTexField(
+            hintText: 'email',
+            controller: _emailcontroller,
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          //password field
+          MyTexField(
+            hintText: 'password',
+            oscureText: true,
+            controller: _passwordcontroller,
+          ),
+          //login
+          const SizedBox(
+            height: 10,
+          ),
+          MyButton(
+            text: 'Login',
+            onTap: login,
+          ),
+          //Register
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Not a member? ',
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            //email field
-            MyTexField(
-              hintText: 'email',
-              controller: _emailcontroller,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            //password field
-            MyTexField(
-              hintText: 'password',
-              oscureText: true,
-              controller: _passwordcontroller,
-            ),
-            //login
-            const SizedBox(
-              height: 10,
-            ),
-            MyButton(
-              text: 'Login',
-              onTap: login,
-            ),
-            //Register
-            const SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  'Not a member? ',
+              GestureDetector(
+                onTap: toggleScreen,
+                child: const Text(
+                  'Register Now',
+                  style: TextStyle(fontWeight: FontWeight.bold),
                 ),
-                GestureDetector(
-                  onTap: toggleScreen,
-                  child: const Text(
-                    'Register Now',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                )
-              ],
-            ),
-          ],
-        ),
+              )
+            ],
+          ),
+        ],
       ),
     );
   }
